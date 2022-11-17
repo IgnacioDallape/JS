56ec347879c6370037d50a21dfcd34e02c1809b5
ignacio <nachodallape2@gmail.com>
2022-11-17 11:03:28 -0300

    js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//ESTO ES UN COMENTARIO "CTRL + K + C", ESTO ES PARA UNA LINEA
+/* ASI SE HACE UN MULTILINEA
+O CON EL ATAJO SELECCIONANDO VARIAS 
+LINEAS */
+
+
+let miNombre = "ignacio"; //declaro la variable y la inicializo.
+let minombre = "igna";  //esta es diferente a la de arriba, ya que la mayuscula modifica todo
+let miApellido = "dallape";
+let minombre
